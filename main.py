2ae03da553ec1d4e80cf16c9c236048502c6ab57
xFaZe341 <86230524+xFaZe341@users.noreply.github.com>
2022-11-26 18:56:37 +0400

    second commit

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+print ("hello world")
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
