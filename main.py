4d06772d6b327356bafc71aba04b92a410526b90
xFaZe341 <86230524+xFaZe341@users.noreply.github.com>
2022-11-26 19:37:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print ("hello world again")
 print ("hello world")
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
